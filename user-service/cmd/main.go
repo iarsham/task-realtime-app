8b1da7303ecdbbc9b2a04d6c881b2240877508ee
Arsham Roshannejad <arshamdev2001@gmail.com>
2024-06-21 20:29:26 +0330

    add:custom zap logger and mongo db connection

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/iarsham/task-realtime-app/user-service/configs"
+	"github.com/iarsham/task-realtime-app/user-service/db"
+	"github.com/iarsham/task-realtime-app/user-service/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	cfg, err := configs.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	logs, err := logger.NewZapLog(cfg.App.Debug)
+	if err != nil {
+		panic(err)
+	}
+	defer logs.Sync()
+
+	mongo, err := db.OpenDB(cfg)
+	if err != nil {
+		logs.Panic("mongo connection failed", zap.Error(err))
+	}
+	defer mongo.Disconnect(context.Background())
+}
