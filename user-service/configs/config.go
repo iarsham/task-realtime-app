79ef55d5fa3f3836a39de859d60156f01e4788ba
Arsham Roshannejad <arshamdev2001@gmail.com>
2024-06-21 20:22:20 +0330

    add:unittest for repository and perform nosql operation

---
+++ user-service/configs/config.go
@@ -0,0 +1,61 @@
+package configs
+
+import (
+	"bytes"
+	_ "embed"
+	"github.com/spf13/viper"
+	"strings"
+)
+
+//go:embed config.yaml
+var content []byte
+
+type Mongo struct {
+	MongoUser  string
+	MongoPass  string
+	MongoHost  string
+	MongoPort  int
+	MongoDB    string
+	Collection string
+}
+
+type Redis struct {
+	Host     string
+	Port     int
+	Password string
+}
+
+type RabbitMQ struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+}
+
+type App struct {
+	Debug           bool
+	Host            string
+	Port            int
+	SecretKey       string
+	TokenExpireHour int
+}
+
+type Config struct {
+	Mongo *Mongo
+	App   *App
+}
+
+func NewConfig() (*Config, error) {
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("ENV")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	viper.SetConfigType("yaml")
+	if err := viper.ReadConfig(bytes.NewBuffer(content)); err != nil {
+		return nil, err
+	}
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
