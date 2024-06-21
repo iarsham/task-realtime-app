bf485691c16cb7f2eb6f6d068ad9ca57595e8ed9
Arsham Roshannejad <arshamdev2001@gmail.com>
2024-06-21 20:33:32 +0330

    fix:missing write start server utils

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/iarsham/task-realtime-app/user-service/configs"
 	"github.com/iarsham/task-realtime-app/user-service/db"
 	"github.com/iarsham/task-realtime-app/user-service/logger"
+	"github.com/iarsham/task-realtime-app/user-service/routers"
 	"go.uber.org/zap"
 )
 
@@ -25,4 +28,14 @@ func main() {
 		logs.Panic("mongo connection failed", zap.Error(err))
 	}
 	defer mongo.Disconnect(context.Background())
+
+	if !cfg.App.Debug {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	gin.ForceConsoleColor()
+	r := gin.Default()
+	routers.SetupRouters(r, mongo.Database(cfg.Mongo.MongoDB), cfg, logs)
+
+	addr := fmt.Sprintf("%s:%d", cfg.App.Host, cfg.App.Port)
+	logs.Fatal("server start failed", zap.Error(r.Run(addr)))
 }
