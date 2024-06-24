f0dddbdcce6e346d795e198be75d0a44f7ece8d6
Arsham <arshamdev2001@gmail.com>
2024-06-24 19:43:33 +0330

    delete:addtional helpers functions

--- user-service/helpers/jwt.go
+++ user-service/helpers/jwt.go
@@ -16,28 +16,3 @@ func CreateAccessToken(user *models.Users, secretKey string, expire int) (string
 	}
 	return jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(secretKey))
 }
-
-func IsTokenValid(reqToken string, secretKey string) (*jwt.Token, error) {
-	token, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, jwt.ErrSignatureInvalid
-		}
-		return []byte(secretKey), nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return token, nil
-}
-
-func GetClaims(token *jwt.Token) (map[string]interface{}, error) {
-	claimsMap := make(map[string]interface{})
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if ok && token.Valid {
-		for k, v := range claims {
-			claimsMap[k] = v
-		}
-		return claimsMap, nil
-	}
-	return nil, jwt.ErrTokenInvalidClaims
-}
